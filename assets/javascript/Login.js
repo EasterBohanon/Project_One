5b046855884e2334c6861fb14bccda48ca71dec8
acerise <42525915+acerise85@users.noreply.github.com>
2019-01-04 12:31:26 -0700

    Favorite button working to pull recipe name into array but not displaying in favorites page

--- assets/javascript/Login.js
+++ assets/javascript/Login.js
@@ -1,56 +1,25 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC2ofFoahSCG7naJIyo7txw0rGVKKxOq5o",
+    authDomain: "driven-copilot-227601.firebaseapp.com",
+    databaseURL: "https://driven-copilot-227601.firebaseio.com",
+    projectId: "driven-copilot-227601",
+    storageBucket: "driven-copilot-227601.appspot.com",
+    messagingSenderId: "973237957612"
+  };
+  firebase.initializeApp(config);
 
-
-
-$(document).ready(function(){
-
-    var config = {
-        apiKey: "AIzaSyBce3_oJFb_y9O6NRAnZft5Ia6dSf3dKL4",
-        authDomain: "myfirstproject-9403c.firebaseapp.com",
-        databaseURL: "https://myfirstproject-9403c.firebaseio.com",
-        projectId: "myfirstproject-9403c",
-        storageBucket: "myfirstproject-9403c.appspot.com",
-        messagingSenderId: "909828001745"
-      };
-      firebase.initializeApp(config);
-
-     
-      var uiConfig = {
-        callbacks: {
-          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-            // User successfully signed in.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
-            return true;
-          },
-          uiShown: function() {
-            // The widget is rendered.
-            // Hide the loader.
-            document.getElementById('loader').style.display = 'none';
-          }
-        },
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-        signInFlow: 'popup',
-        signInSuccessUrl: '<url-to-redirect-to-on-success>',
-        signInOptions: [
-          // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.PhoneAuthProvider.PROVIDER_ID
-        ],
-        // Terms of service url.
-        tosUrl: '<your-tos-url>',
-        // Privacy policy url.
-        privacyPolicyUrl: '<your-privacy-policy-url>'
-      };
-
-      var ui = new firebaseui.auth.AuthUI(firebase.auth());
-      ui.start('#firebaseui-auth-container', {
-        signInOptions: [
-          firebase.auth.EmailAuthProvider.PROVIDER_ID
-        ],
-        // Other config options...
-      });
-})
+  
+    var uiConfig = {  
+     signInSuccessUrl: 'index.html',  
+     signInOptions: [  
+      // Specify providers you want to offer your users.  
+    //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,  
+      firebase.auth.EmailAuthProvider.PROVIDER_ID  
+     ],  
+       
+    };  
+    // Initialize the FirebaseUI Widget using Firebase.  
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());  
+    // The start method will wait until the DOM is loaded.  
+    ui.start('#firebaseui-auth-container', uiConfig);  
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -30,6 +30,7 @@ $(document).ready(function () {
 
 
     // Global Variables
+    var favorite = [];
     var search = {};
     var recipe = {};
     var key = '6c1aa41e76cc55600f7a88e531724d23'; // Chris's Yummly API key
@@ -125,7 +126,7 @@ $(document).ready(function () {
                 this.name = response.name;
                 this.yield = response.yield;
                 this.source = response.source;
-                this.ingredientLines = response.ingredientLines;
+                this.ingredientLines = response.ingredientLines; //use this for shopping cart
                 this.numberOfServings = response.numberOfServings;
                 this.totalTimeInSeconds = response.totalTimeInSeconds;
                 this.attributes = response.attributes;
@@ -150,30 +151,16 @@ $(document).ready(function () {
         // 1) Assign new search object
         search = new Search(query);
 
-
         if (!bool) {
             $('#recipes_view').empty();
             $('.num_results').empty();
         }
         // 2) Prepare UI for recipes
 
-
-        // 2) Prepare UI for recipes
-        $('#recipes_view').empty()
-
-      //on click of user recipie submit button
-      $(document).ready(function(){
-        $('select').formSelect();
-
-
         // Render the preloader
         renderLoader(true);
 
-
         // 3) Call getResult method to return API response consisting of recipes
-
-        // 3) 
-
         search.getResult(query)
 
             // If API request successful
@@ -195,7 +182,7 @@ $(document).ready(function () {
             .fail(function (error) {
                 displayNoResults();
             });
-    });
+    };
 
 
     // Controls all recipe tasks
@@ -421,8 +408,25 @@ $(document).ready(function () {
     $(document).on('click', '.recipe_result', function () {
         var id = $(this).attr('data-recipeid');
         recipeController(id);
-    });
+    }); 
+    
+    // favorite recipe button 
+    $(document).on('click', '.favButton', function () {
+        var id = $(this).attr('data-recipeName');
+        recipeController(id);
+        // recipe.getRecipe()
+        favorite.push(recipe.name);
+        for(i = 0; i < favorite.length; i++){
+        var favRecipe = recipe.name;
+        
+        // favRecipe.attr("data-recipeName", favorite[i]);
+        // favRecipe.text("#favRecipe");
+        $("#favDisplay").append(favRecipe);
+    }
+        // favorite.push(recipe.name);
 
+        console.log(favorite);
+    });
 
     // Search Keypress Listener
     $('#search_form').keypress((e) => {
@@ -618,77 +622,4 @@ $(document).ready(function () {
      * 
      * 
      */
-
-
-
-
-
-
-
-
-
-    /*****************  Below are Possible methods for autocomplete 
-     * 
-     * when a user enters in ingredients to include / exclude
-     * 
-     */
-
-
-
-    // jquery search autocomplete reference
-    // https://github.com/devbridge/jQuery-Autocomplete
-
-
-
-    // $('input.autocomplete').autocomplete({
-    //     data: {
-    //         "Apple": null,
-    //         "Microsoft": null,
-    //         "Google": 'https://placehold.it/250x250'
-    //     },
-    //     limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
-    //     onAutocomplete: function (val) { // Callback function when value is autcompleted.
-
-    //     },
-    //     minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
-    // });
-
-        console.log("This is the recipie search button");
-        
-
-
-        // //Perform Ajax Call
-        // $.ajax({
-        //     url: "http://api.yummly.com/v1/api/recipes?_app_id=fa6a5b04$_app_key=4d7d57aa09b888bc12e36f57b76129bc&" + input,
-        //     method: "GET"
-        // }).then(function(response){
-
-        //     var results = response.data;
-
-
-        // })
-
-      };
-
-      
-
-    // $(function () {
-    //     $.ajax({
-    //         type: 'GET',
-    //         url: '',
-    //         success: function (response) {
-    //             var countryArray = response;
-    //             var dataCountry = {};
-    //             for (var i = 0; i < countryArray.length; i++) {
-    //                 //console.log(countryArray[i].name);
-    //                 dataCountry[countryArray[i].name] = countryArray[i].flag; //countryArray[i].flag or null
-    //             }
-    //             $('input.autocomplete').autocomplete({
-    //                 data: dataCountry,
-    //                 limit: 5, // The max amount of results that can be shown at once. Default: Infinity.
-    //             });
-    //         }
-    //     });
-    // });
-
 });
